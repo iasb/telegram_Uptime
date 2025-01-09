5cdf271a56aa23e7ef8361046cbead17297afaaf
iasb <52027240+iasb@users.noreply.github.com>
2025-01-09 17:06:02 +0200

    Update Telegram_Uptime.py

--- Telegram_Uptime.py
+++ Telegram_Uptime.py
@@ -8,18 +8,14 @@ import telepot
 import time, datetime
 import os
 import random
-#import subprocess
 
 from telepot.loop import MessageLoop
 from re import findall
-#from subprocess import Popen, PIPE
-#from pythonping import ping
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
 now = datetime.datetime.now()
 
-#LED
 bot = telepot.Bot('________')
 
 chat_id = ________
@@ -28,7 +24,7 @@ chat_id = ________
 
 output = 'test'
 mylist = []
-#today = datetime.date.today()
+
 today = datetime.datetime.now()
 
 mylist.append(today)
@@ -38,16 +34,12 @@ mylist.append(today)
 
 out_start = today.strftime('%d/%m/%Y')+' ' + today.strftime('%H:%M')
 
-#output = today.strftime('%d/%m/%Y')+' ' + today.strftime('%H:%M:%S')
+
 
 
 #print('output ' + output)
 
 while 1:
-
-#  output = today.strftime('%d/%m/%Y')
-#  output2 = today.strftime('%H:%M:%S')
-
 
   today = datetime.datetime.now()
 
@@ -55,8 +47,6 @@ while 1:
 
 #  print('output2 ' + output2)
 
-  # Send the output to the chat
-#  im = bot.sendMessage(chat_id, text=out_start+'  -->  '+output+' '+output2)
 
   im = bot.sendMessage(chat_id, text='1.18:   '+out_start+'  -->  '+output2)
 
